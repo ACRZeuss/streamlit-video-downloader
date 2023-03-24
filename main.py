bdc37c6c4ce8a959d80e22509a550cd263f10a27
Erhan <ep4963807@gmail.com>
2023-03-24 22:23:11 +0300

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title = "Video Downloader by Erhan"
 
 if len(url) > 0:
     yt = YouTube(url)
-    result = YouTube(url).streams.get_highest_resolution().download()
+    result = yt.streams.get_highest_resolution().download()
 
     with open(result, 'rb') as file:
         btn = st.download_button(
